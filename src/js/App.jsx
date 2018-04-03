e85c164cccb7df2f2fb8978127f4922a4a92a642
sla16 <samla.sla94@gmail.com>
2018-04-02 22:53:50 -0400

    Adds some color

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 
 import NavBar from 'js/controls/NavBar';
+
+import github from 'images/github.png';
 import 'css/App.css';
 
 export default class App extends Component {
@@ -10,8 +12,29 @@ export default class App extends Component {
 
   render () {
     return (
-      <div className='content'>
-        <NavBar />
+      <div className='App_content'>
+        <div className='App_navBar'>
+          <NavBar />
+        </div>
+
+        <div className='App_logo App_logo-main'>
+        </div>
+
+        <div id='about' className='App_about'>
+        </div>
+
+        <div id='other' className='App_logo App_logo-other'>
+        </div>
+
+        <div className='App_other'>
+        </div>
+
+        <div id='contact' className='App_contact'>
+          <a href='https://github.com/sla16/samla' className='App_icon' target='_blank'><img alt='https://github.com/sla16/samla' src={github} /></a>
+        </div>
+
+        <div className='App_footer'>
+        </div>
       </div>
     )
   }
--- src/js/controls/NavBar.jsx
+++ src/js/controls/NavBar.jsx
@@ -1,19 +1,19 @@
 import React, {Component} from 'react';
 
-import github from 'images/github.png';
 import 'css/controls/NavBar.css';
 
 export default class NavBar extends Component {
   constructor (props) {
     super(props);
-    console.log(github);
   }
 
   render () {
     return (
       <div className='navBar'>
-        <a href='#' className='navOption'>HOME</a>
-        <a href='#' className='navOption navIcon'><img alt='GitHub' src={github} /></a>
+        <a href='#' className='navOption'>Home</a>
+        <a href='#about' className='navOption'>About</a>
+        <a href='#other' className='navOption'>Other</a>
+        <a href='#contact' className='navOption'>Contact</a>
       </div>
     )
   }
